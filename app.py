72878e2317074298371e471663a4ae9373c53dd3
Thiyaku <thiyakuso4@gmail.com>
2024-11-05 12:53:11 +0530

    Added print

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def get_vector(uploaded_file, out_dir=None):
     out_file = os.path.join(out_dir, uploaded_file.name)
     with open(out_file, "wb") as fp:
         fp.write(content)
-
+    st.write(content)
     if uploaded_file.name.endswith(".zip"):
         out_name = uploaded_file.name[:-4]
         with zipfile.ZipFile(out_file, "r") as zip_ref:
