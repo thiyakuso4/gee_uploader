21f7b9f6200b96db976b89f3529be713dbf090d7
Thiyaku <thiyakuso4@gmail.com>
2024-11-15 13:47:34 +0530

    Added print for debug

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ def get_vector(uploaded_file, out_dir=None):
                 if files:
                     vector = geemap.geojson_to_ee(files[0])
     elif uploaded_file.name.endswith(".kml"):
+            st.write(f'Here we go {uploaded_file.name}')
             out_name = uploaded_file.name.replace('.kml', "")
             vector = geemap.kml_to_ee(out_file)
     else:
